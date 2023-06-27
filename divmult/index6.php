57d1c5c73f4599f1e1e10890f35bd6eaa3af8bee
Hosana Silvino <89276489+Hosana-Silvino@users.noreply.github.com>
2023-06-27 20:45:16 -0300

    Add files via upload

---
+++ divmult/index6.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    
+    <title>Soma e Multiplicação</title>
+</head>
+<body>
+
+        <h1> Soma de ímpares e Multiplicação de pares de 0 a 20</h1>
+    <form method="post" action="divmult.php">
+
+    
+    <br>
+  
+     <input type="submit" value="Calcular">
+
+
+    </form>
+</body>
+</html>
